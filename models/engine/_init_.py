d345c19c09084c8612ef9672f437099a8b226751
bandanoah <bandahnoah007@gmail.com>
2024-11-18 12:52:02 +0300

    changes

--- models/engine/_init_.py
+++
@@ -1,7 +0,0 @@
-from models.engine.file_storage import FileStorage
-
-# Create a single instance of FileStorage
-storage = FileStorage()
-
-# Load any objects from the JSON file into memory
-storage.reload()
